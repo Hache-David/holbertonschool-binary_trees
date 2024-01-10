b57cd71fc084187943b1add4a91e19187a4daf80
Hache-David <Hache-David@github.com>
2024-01-10 14:42:07 +0000

    fix exercice 16 with new

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Calcule la hauteur d'un arbre binaire
- * @tree: Pointeur vers le nœud racine de l'arbre à mesurer
- * Return: La hauteur de l'arbre
+ * binary_tree_is_perfect - Vérifie si un arbre binaire est parfait
+ * @tree: Pointeur vers le nœud racine de l'arbre à vérifier
+ * Return: 1 si l'arbre est parfait, sinon 0
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
@@ -13,7 +13,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height + 1 : right_height + 1);
+	if (left_height == right_height)
+		return (binary_tree_is_full(tree));
+
+	return (0);
 }
 
 /**
@@ -36,11 +39,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_perfect - Vérifie si un arbre binaire est parfait
- * @tree: Pointeur vers le nœud racine de l'arbre à vérifier
- * Return: 1 si l'arbre est parfait, sinon 0
+ * binary_tree_height - Calcule la hauteur d'un arbre binaire
+ * @tree: Pointeur vers le nœud racine de l'arbre à mesurer
+ * Return: La hauteur de l'arbre
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
@@ -48,8 +51,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 
-	if (left_height == right_height)
-		return (binary_tree_is_full(tree));
-
-	return (0);
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
+
+
+
+
