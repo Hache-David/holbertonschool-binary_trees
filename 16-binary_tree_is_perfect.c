a6d778aae7a08354ebed304c767a45ad562960ac
Hache-David <Hache-David@github.com>
2024-01-11 12:12:00 +0000

    Fix compilation issue with correct variable declaration.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,11 +7,13 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height == right_height)
 		return (binary_tree_is_full(tree));
@@ -45,15 +47,13 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
-
-
-
-
